535aeee3da2c8b3ff66e2246e02966ad65e99168
tom <tomaz413gr@mail.ru>
2023-04-08 16:52:37 +1000

    02_basket and object

--- Галянская/roll_basket.js
+++ Галянская/roll_basket.js
@@ -3,7 +3,14 @@ window.addEventListener('click', function (event) {
         const card = event.target.closest('.cont');
         const productInfo = {
             id: card.dataset.id,
-            imgsrc: 
-        }
+            imgSrc: card.querySelector('.product-image').getAttribute('src'),
+            title: card.querySelector('.item-title').innerText,
+            itensInBox: card.querySelector('[data-itens-in-box]').innerText,
+            weight: card.querySelector('.price_weight').innerText,
+            price: card.querySelector('.price_currency').innerText,
+            counter: card.querySelector('[data-counter]').innerText,
+        };
+        console.log(productInfo);
+        console.log('gjnkgj');
     }
 })
