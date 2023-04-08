145294396b68dc9294d44237da6705aab8ea8f2d
tom <tomaz413gr@mail.ru>
2023-04-08 15:30:31 +1000

    01_basket

---
+++ Галянская/roll_basket.js
@@ -0,0 +1,9 @@
+window.addEventListener('click', function (event) {
+    if (event.target.hasAttribute('data-cart')) {
+        const card = event.target.closest('.cont');
+        const productInfo = {
+            id: card.dataset.id,
+            imgsrc: 
+        }
+    }
+})
