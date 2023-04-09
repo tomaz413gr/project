0c05ce862ad978a6a93f7701f8cabd4c2f0e9e2d
tomaz413gr <123848400+tomaz413gr@users.noreply.github.com>
2023-04-09 10:20:05 +1000

    add to basket++

--- Галянская/roll_basket.js
+++ Галянская/roll_basket.js
@@ -1,18 +1,25 @@
-const basket = document.querySelector('.basket_content');
+const basket = document.querySelector(".basket_content");
 
-window.addEventListener('click', function (event) {
-    if (event.target.hasAttribute('data-cart')) {
-        const card = event.target.closest('.cont');
-        const productInfo = {
-            id: card.dataset.id,
-            imgSrc: card.querySelector('.product-image').getAttribute('src'),
-            title: card.querySelector('.item-title').innerText,
-            itensInBox: card.querySelector('[data-itens-in-box]').innerText,
-            weight: card.querySelector('.price_weight').innerText,
-            price: card.querySelector('.price_currency').innerText,
-            counter: card.querySelector('[data-counter]').innerText,
-        };
-        const cartItemHtml = `<div class="basket" data-id="${productInfo.id}">
+window.addEventListener("click", function (event) {
+  if (event.target.hasAttribute("data-cart")) {
+    const card = event.target.closest(".cont");
+    const productInfo = {
+      id: card.dataset.id,
+      imgSrc: card.querySelector(".product-image").getAttribute("src"),
+      title: card.querySelector(".item-title").innerText,
+      itensInBox: card.querySelector("[data-itens-in-box]").innerText,
+      weight: card.querySelector(".price_weight").innerText,
+      price: card.querySelector(".price_currency").innerText,
+      counter: card.querySelector("[data-counter]").innerText,
+    };
+    const itemBasket = basket.querySelector(`[data-id="${productInfo.id}"]`);
+    console.log(itemBasket);
+    if (itemBasket) {
+      const dataCounter = itemBasket.querySelector("[data-counter]");
+      dataCounter.innerText =
+        parseInt(dataCounter.innerText) + parseInt(productInfo.counter);
+    } else {
+      const cartItemHtml = `<div class="basket" data-id="${productInfo.id}">
         <div class="item_img"><img src=${productInfo.imgSrc} alt="">></div>
         <div class="item_info">
             <h5>${productInfo.title}</h5>
@@ -25,6 +32,7 @@ window.addEventListener('click', function (event) {
             </div>
         </div>
     </div>`;
-    basket.insertAdjacentHTML('beforeend',cartItemHtml);
+      basket.insertAdjacentHTML("beforeend", cartItemHtml);
     }
-})
+  }
+});
