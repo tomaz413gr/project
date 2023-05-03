ee3100a83f548ee4e4b6ffe17f1177d7c59397ca
tom <tomaz413gr@mail.ru>
2023-05-03 17:43:19 +1000

    багет с иконками svg-анимация

--- Галянская/anim.js
+++ Галянская/anim.js
@@ -3,18 +3,13 @@ const anim_circle2 = document.querySelector('.anim_circle2');
 const anim_circle3 = document.querySelector('.anim_circle3');
 const anim_circle4 = document.querySelector('.anim_circle4');
 const clickCircle = document.querySelector('.click_circle');
-clickCircle.addEventListener('click', function (){
-    if (animCircle.classList.contains('move') && anim_circle2.classList.contains('move2') && anim_circle3.classList.contains('move3') && anim_circle4.classList.contains('move4')){
-        animCircle.classList.remove('move');
-        anim_circle2.classList.remove('move2');
-        anim_circle3.classList.remove('move3');
-        anim_circle4.classList.remove('move4');
+clickCircle.addEventListener('click', function () {
 
-    } else {
-        animCircle.classList.add('move');
-        anim_circle2.classList.add('move2');
-        anim_circle3.classList.add('move3');
-        anim_circle4.classList.add('move4');
-        
-    }
+    animCircle.classList.toggle('move');
+    anim_circle2.classList.toggle('move2');
+    anim_circle3.classList.toggle('move3');
+    anim_circle4.classList.toggle('move4');
 })
+document.querySelector('button').addEventListener('click', function(){
+    document.querySelector('#SVGmator__c1dux1560067354551').contentWindow.postMessage('play', '*')
+  });
