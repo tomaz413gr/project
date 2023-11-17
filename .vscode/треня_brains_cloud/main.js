add3c8569ae141318e3b4c39c5fd2c36590d645b
tomaz413gr <123848400+tomaz413gr@users.noreply.github.com>
2023-11-17 20:32:39 +1100

    треня_brains_cloud_box-sizing

--- .vscode/треня_brains_cloud/main.js
+++ .vscode/треня_brains_cloud/main.js
@@ -1,5 +1,38 @@
-$(function(){
+$(function () {
     let header = $('#header');
     let intro = $('#intro');
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    scrollTech(scrollPos, introH);
+
     header.addClass('fixed');
+    $(window).on("scroll resize", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+        console.log(scrollPos);
+        scrollTech(scrollPos, introH);
+
+    });
+
+    function scrollTech(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    $('[data-scroll]').on('click', function (event) {
+        event.preventDefault();
+        let elementId = $(this).data('scroll');
+        console.log(elementId);
+        let elementOffset = $(elementId).offset().top;
+        $("html, body").animate({
+            scrollTop: elementOffset
+        }), 700;
+
+    });
+
+
+
 })
