2bd16cc2bf5a2706372d218dee88dfeabce3b572
tomaz413gr <123848400+tomaz413gr@users.noreply.github.com>
2023-12-03 14:14:59 +1100

    ПЕРЕТАСКИВАНИЕ ЭЛЕМЕНТОВ

--- .vscode/Castaway/main.js
+++ .vscode/Castaway/main.js
@@ -1,9 +1,19 @@
-$(document).ready(function(){
-$('.block__title').click(function (event) {
-    if ($('.inner__block').hasClass('one')){
-        $('.block__title').not($(this)).removeClass('active');
-        $('.block__text').not($(this).next()).slideUp(300);
-    }
-    $(this).toggleClass('active').next().slideToggle(300);
-});
-});
+const zone__1 = document.querySelector('.zone__1');
+const zone__2 = document.querySelector('.zone__2');
+const ufo = document.querySelector('#ufo');
+zone__1.ondragover = allowDrop;
+zone__2.ondragover = allowDrop;
+function allowDrop(event) {
+    event.preventDefault();
+};
+    ufo.ondragstart = drag;
+    function drag(event) {
+        event.dataTransfer.setData('id', event.target.id);
+    };
+    zone__1.ondrop = drop;
+    zone__2.ondrop = drop;
+    function drop(event) {
+let itemId = event.dataTransfer.getData('id');
+event.target.append(document.getElementById(itemId));
+    };
+
