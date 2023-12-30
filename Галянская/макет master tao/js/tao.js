ecc1743382acf64b8aacb1ce8fb516c3b79ea9b2
tomaz413gr <123848400+tomaz413gr@users.noreply.github.com>
2023-12-30 15:58:29 +1100

    tabs

--- Галянская/макет master tao/js/tao.js
+++ Галянская/макет master tao/js/tao.js
@@ -84,4 +84,18 @@ const burger = document.querySelector('.burger');
 const burgerMenu = document.querySelector('.burger__menu');
 burger.addEventListener('click', ()=> {
     burgerMenu.classList.toggle('menu__open');
+
+    //Tabs
+
+    
 })
+
+$(document).ready(function(){
+    $('.block__title').click(function (event) {
+        if ($('.inner__block').hasClass('one')){
+            $('.block__title').not($(this)).removeClass('active');
+            $('.block__text').not($(this).next()).slideUp(300);
+        }
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+    });
